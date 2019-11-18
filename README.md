3a455a08a37af33e74702028ab781b9b2ebda685
Kevin <k.amaral@outlook.fr>
2019-11-19 00:02:11 +0100

    algo
    algorithmic part terminate

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ algorithm course
 - The clock can display the hours from 00:00:00:00 to 23:59:59:59
 - The clock takes 6 random numbers between 0 and 9 as input
 - With this the clock must display N/A (88:88:88:88) or the smallest possible time
-example 54 12 01 becomes 01:12:12:45
+example 54 12 01 becomes 01:12:45
--- js/script.js
+++ js/script.js
@@ -1,5 +1,87 @@
-function js_alert (){
-    var a = document.getElementById("digit_1").value; 
-    console.log(a);
-    alert(a);
+function js_alert() {
+    var array_digit = [];
+    var i = 1;
+    while (i < 7) {
+        var tmp = document.forms["form_digit"].elements["digit_" + i].value
+        array_digit[i - 1] = tmp;
+        i++;
+    }
+    array_crossing(array_digit);
+    clock(array_digit);
+}
+
+function array_crossing(array) {
+    var i = 0;
+    while (i < 7) {
+        if (array[i] > array[i + 1]) {
+            var tmp = 0;
+            tmp = array[i];
+            array[i] = array[i + 1]
+            array[i + 1] = tmp;
+            i = 0;
+        } else {
+            i++;
+
+        }
+    }
+    return array;
+}
+
+function clock(array_clock) {
+    alert(array_clock);
+    var hour = [];
+    var i = 1;
+    if (i == 1) {
+        if (array_clock[0] < 3) {
+            hour.push(array_clock[0]);
+            alert(hour[0] + "0:00:00");
+            i++;
+        } else {
+            alert("N/A");
+        }
+
+    }
+    if (i == 2) {
+        if (hour[0] < 2) {
+            hour.push(array_clock[1]);
+            alert(hour[0] + hour[1] + ":00:00");
+            i++
+        } else {
+            if (array_clock[1] < 4) {
+                hour.push(array_clock[1]);
+                alert(hour[0] + hour[1] + ":00:00");
+                i++;
+            } else {
+                alert("N/A");
+            }
+        }
+        if (i == 3) {
+            if (array_clock[2] < 7) {
+                hour.push(array_clock[2]);
+                alert(hour[0] + hour[1] + ":" + hour[2] + "0:00");
+                i++;
+            } else {
+                alert("N/A");
+            }
+        }
+        if (i == 4) {
+            hour.push(array_clock[3]);
+            alert(hour[0] + hour[1] + ":" + hour[2] + hour[3] + ":00");
+            i++;
+        }
+        if (i == 5) {
+            if (array_clock[4] < 7) {
+                hour.push(array_clock[4]);
+                alert(hour[0] + hour[1] + ":" + hour[2] + hour[3] + ":" + hour[4] + "0");
+                i++;
+            } else {
+                alert("N/A");
+            }
+        }
+        if (i == 6) {
+            hour.push(array_clock[5]);
+            alert(hour[0] + hour[1] + ":" + hour[2] + hour[3] + ":" + hour[4] + hour[5]);
+            i++;
+        }
+    }
 }
